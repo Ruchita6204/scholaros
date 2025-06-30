089f68f3cc5d6a500cb1192599d28fdad7467c97
Ruchita6204 <itsmeruchita6204@gmail.com>
2025-06-30 15:53:55 +0530

    Add website files

---
+++ script.js
@@ -0,0 +1,8 @@
+function startPreparing() {
+  alert("Redirecting to preparation modules...");
+}
+
+function toggleMenu() {
+  const navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+}
